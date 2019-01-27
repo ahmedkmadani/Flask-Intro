6ed09161de5809ff5ff45c3f03f35135f9a8dff6
[ahmedkmadani] <[ahmedkamal543@gmail.com]>
2019-01-27 12:13:59 +0000

    working with joint

--- run_app.py
+++ run_app.py
@@ -10,7 +10,10 @@ import os
 # from library._08_redirects import app
 # from library._09_simple_database_app import app
 #from library._10_database_app_template_eng import app
-from library._11_database_app_template_conditional import app
+# from library._11_database_app_template_conditional import app
+from library._12_database_app_with_join import app
+# from library._13_simple_form_submission import app
+
 
 
 if __name__== '__main__':
